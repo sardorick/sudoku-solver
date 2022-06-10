a174d9a13d700f898cc70f9296218a9429997af2
Felix Schekerka <felixschekerka@gmail.com>
2022-06-10 16:28:28 +0200

    cropping and sorting

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,29 @@
+from os import pread
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
 
 def preprocess_image(path):
     kernel=cv2.getStructuringElement(cv2.MORPH_CROSS, (10,10))
@@ -27,13 +50,27 @@ def preprocess_image(path):
 
 def crop_squares(contours, path):
     img = cv2.imread(path)
-    x,y,w,h = cv2.boundingRect(contours[2])
     copy=img.copy()
-    cv2.rectangle(copy, (x,y), (x+w, y+h), (0,255,0), 2)
-    return copy
+    cells = []
+    l_r_contours, _ = sort_contours(contours, method="top-to-bottom")
+    for i in range(0,len(l_r_contours), 1):
+        x,y,w,h = cv2.boundingRect(l_r_contours[i])
+        coord = [x, y, w, h]
+        # square_coords.append(coord)
+        cell = img[y:y+h, x:x+w]
+        cells.append(cell)
+
+        # cv2.rectangle(copy, (x,y), (x+w, y+h), (0,255,0), 2)
+
+
+
+
+    # cv2.rectangle(copy, (x,y), (x+w, y+h), (0,255,0), 2)
+    # return l_r_contours
+    return square_coords
  
 
-
+# print(crop_squares(preprocess_image('sudoku_unsolved.png'), 'sudoku_unsolved.png'))
 cv2.imwrite('preprocessed_output.png', crop_squares(preprocess_image('sudoku_unsolved.png'), 'sudoku_unsolved.png'))
 
 
